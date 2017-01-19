a8a7661967058c19ac8fbb387231b896a40cd090
lz1024 <chinalzge@gmail.com>
2017-01-19 16:42:03 +0800

    运行时注解

--- Inject_IOC1/inject_ioc2_onclick/src/main/java/com/lz/inject_ioc2_onclick/MainActivity.java
+++ Inject_IOC1/inject_ioc2_onclick/src/main/java/com/lz/inject_ioc2_onclick/MainActivity.java
@@ -2,7 +2,6 @@ package com.lz.inject_ioc2_onclick;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -27,9 +26,14 @@ public class MainActivity extends AppCompatActivity {
         ViewInjectUtils.inject(this);
     }
 
+    /**
+     * 注解点击事件
+     *
+     * @param view
+     */
     @OnClick({R.id.button1, R.id.button2})
     public void initClickBtnInvoKed(View view) {
-        Log.e("TAG", "点击了");
+        //        Log.e("TAG", "点击了");
         switch (view.getId()) {
             case R.id.button1:
                 Toast.makeText(this, "点击了按钮1", Toast.LENGTH_SHORT).show();
